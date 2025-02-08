d7a2a12b2ba9b1807c1d528b4f38d2ca7f6b9154
dksgusdn61 <dksgusdn61@gmail.com>
2025-02-09 04:34:04 +0900

    Fix: 여러가지 변경사항 및 불필요한 import 제거

--- src/shared/ui/SummationElement.tsx
+++ src/shared/ui/SummationElement.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import styled from 'styled-components';
 import { Typography } from './Typography';
-import { Icon } from './Icon';
 import { ArrowIcon } from './ArrowIcon';
 
 const SummationElement = ({

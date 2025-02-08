72b68382c211f042b47346274d9d9ed69ce1dc54
Tech5818 <pqow5818@gmail.com>
2025-02-09 01:36:07 +0900

    Feat: Routing

---
+++ src/pages/profile/ui/index.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const ProfilePage = () => {
+  return (
+    <>
+      <Container></Container>
+    </>
+  );
+};
+
+const Container = styled.div``;
